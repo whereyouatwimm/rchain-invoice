207bf69042b963bbf54e605ec3bc30394674c5c5
wimm <35243180+whereyouatwimm@users.noreply.github.com>
2018-09-10 19:49:46 +0000

    Fixed issues with the data label of the invoice identifier
    the invoice_identifier label wasn't able to pull the correct invoice data from invoices_summary because it was looking up the label "invoice_id" instead of "invoice_identifier"

--- server.js
+++ server.js
@@ -30,28 +30,26 @@ app.use(express.static('public'));
 
 
 // Contributor agreement endpoints:
-app.get ('/invoices/:invoice_id', function (request, response) {
-  var invoice_id = request.params.invoice_id;
+app.get ('/invoices/:invoice_identifier', function (request, response) {
+  var invoice_identifier = request.params.invoice_identifier;
 
-  // TODO: look up invoice_id in invoice_summary, return data in a json object as response_variables
+  // TODO: look up invoice_identifier in invoice_summary, return data in a json object as response_variables
     invoices.returnWorksheetJson('1DOwdYxhKIvJuYgQe4xv6-7gZ35bjyNZoQ5uHviFYoaA', 'invoices_summary', function (err, jsonArray){
     if (err) { 
-      console.error('an error occured while pulling the contributor data');
+      console.error('an error occured while pulling invoices_summary data');
       return;
     }
     // response.setHeader('Content-Type', 'application/json');
     for (x=0; x < jsonArray[0].length; x++) {
-      if (jsonArray[0][x]['invoice_id'] == invoice_id) {
+      if (jsonArray[0][x]['invoice_identifier'] == invoice_identifier) {
         console.log(jsonArray[0][x]);
-        //console.log(jsonArray[0])
-        
         var response_variables = jsonArray[0][x];
         break
       } 
 
     }
     if ( response_variables == undefined) {
-
+      console.log(jsonArray[0])
       console.log('Invoice ID: ', invoice_id, ' was not found.');
       // Decide what to render here. probably a 404 page.
 
@@ -63,16 +61,16 @@ app.get ('/invoices/:invoice_id', function (request, response) {
 
 
 // this renders the agreement page.
-app.get ('/invoices/:invoice_id/agree', function (request, response) {
-  console.log('user agrees ', request.params.invoice_id);
+app.get ('/invoices/:invoice_identifier/agree', function (request, response) {
+  console.log('user agrees ', request.params.invoice_identifier);
   // record agreement and send out confirmation email here.
 
   response.render('contributor-agrees');
 });
 
 // this receives data from the agree button.
-app.post ('/invoices/:invoice_id/agree', function (request, response) {
-  console.log('user agrees: ', request.params.invoice_id);
+app.post ('/invoices/:invoice_identifier/agree', function (request, response) {
+  console.log('user agrees: ', request.params.invoice_identifier);
   console.log(request.body);
   
   // TODO: 
@@ -87,8 +85,8 @@ app.post ('/invoices/:invoice_id/agree', function (request, response) {
 });
 
 
-app.get ('/invoices/:invoice_id/disagree', jsonParser, function (request, response) {
-  console.log('user disagrees ', request.params.invoice_id);
+app.get ('/invoices/:invoice_identifier/disagree', jsonParser, function (request, response) {
+  console.log('user disagrees ', request.params.invoice_identifier);
   console.log(request.body);
   // record disagreement here if necessary.
     
