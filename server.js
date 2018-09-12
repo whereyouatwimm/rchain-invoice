aa98c0851c8b4ebdb93ccadf6b7facc7780219f7
wimm <35243180+whereyouatwimm@users.noreply.github.com>
2018-09-12 03:13:18 +0000

    Removed unnecessary /invoices/:invoice_id route
    The front end is set up to send a post request to /invoices/:invoice_id/agree containing it's agreement / agreement details. The post request renders the agreement page, so the get route isn't necessary.

--- server.js
+++ server.js
@@ -33,13 +33,13 @@ app.use(express.static('public'));
 app.get ('/invoices/:invoice_identifier', function (request, response) {
   var invoice_identifier = request.params.invoice_identifier;
 
-  // TODO: look up invoice_identifier in invoice_summary, return data in a json object as response_variables
+  // look up invoice_identifier in invoice_summary, return data in a json object as response_variables
     invoices.returnWorksheetJson('1DOwdYxhKIvJuYgQe4xv6-7gZ35bjyNZoQ5uHviFYoaA', 'invoices_summary', function (err, jsonArray){
     if (err) { 
       console.error('an error occured while pulling invoices_summary data');
       return;
     }
-    // response.setHeader('Content-Type', 'application/json');
+
     for (x=0; x < jsonArray[0].length; x++) {
       if (jsonArray[0][x]['invoice_identifier'] == invoice_identifier) {
         console.log(jsonArray[0][x]);
@@ -48,7 +48,7 @@ app.get ('/invoices/:invoice_identifier', function (request, response) {
       } 
 
     }
-    if ( response_variables == undefined) {
+    if (response_variables == undefined) {
       console.log(jsonArray[0])
       console.log('Invoice ID: ', invoice_identifier, ' was not found.');
       // Decide what to render here. probably a 404 page.
@@ -59,14 +59,6 @@ app.get ('/invoices/:invoice_identifier', function (request, response) {
   });
 });
 
-
-// this renders the agreement page.
-app.get ('/invoices/:invoice_identifier/agree', function (request, response) {
-  console.log('user agrees ', request.params.invoice_identifier);
-  // record agreement and send out confirmation email here.
-
-  response.render('contributor-agrees');
-});
 
 // this receives data from the agree button.
 app.post ('/invoices/:invoice_identifier/agree', function (request, response) {
