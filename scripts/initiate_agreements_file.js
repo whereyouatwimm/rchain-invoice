a69675c94f6fd916519acd247d40ecae59a48110
wimm <35243180+whereyouatwimm@users.noreply.github.com>
2018-09-24 21:38:39 +0000

    remove unnecessary comments from initiate_agreements_file.js

--- scripts/initiate_agreements_file.js
+++ scripts/initiate_agreements_file.js
@@ -3,20 +3,7 @@ const csv = require('csvtojson');
 
 // Usage: node initiate_agreements_file.js <filename of csv>
 
-
-// NOTES:
-// This script creates the json file that records agreements by users.
-// It pulls from gsheets, and stores hashed user data in ../data/<pay_period>-contributor.json.
-
-
-// TODO: 
-// replace with csv to json
-
-
 const csvFile = process.argv[2];
-
-// read csv from file to json array
-
 
 csv()
   .fromFile(csvFile)
