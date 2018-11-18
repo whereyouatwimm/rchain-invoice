aba42b018cf53c506d9e333b57067c24f5b71def
wimm <35243180+whereyouatwimm@users.noreply.github.com>
2018-11-18 17:49:42 +0000

    Added fix for permissions issue on the agreementLog.json file.

--- scripts/initiate_agreements_file.js
+++ scripts/initiate_agreements_file.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const csv = require('csvtojson');
+const { exec } = require('child_process');
 
 // Usage: node initiate_agreements_file.js <filename of csv>
 
@@ -13,7 +14,16 @@ csv()
       if(err){ 
         console.log('an error occured saving contributor data: ', err)
       }
-      console.log('the file has been written.')
+        // modify permissions here.
+	console.log('the file has been written.')
+	exec('chown invoice:invoice ../data/agreementLog.json', (err, stdout, stderr) => {
+	if (err) {
+		console.log('Error changing permissions ', err);
+		return;
+	}
+	console.log('Successfully changed permissions on the agreementFile.');
+});
     })
 })
+
 
